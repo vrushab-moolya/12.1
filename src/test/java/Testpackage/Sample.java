dcb78a539ee25d8bf4f48afa0545334cd58ea7f5
vrushab-moolya <vrushab.patil@moolya.com>
2020-12-26 14:43:06 +0530

    second commit

--- src/test/java/Testpackage/Sample.java
+++ src/test/java/Testpackage/Sample.java
@@ -2,6 +2,10 @@ package Testpackage;
 
 import api.Apimethods;
 import base.Basemethods;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.junit.Test;
 import util.Utilmethods;
 
@@ -16,5 +20,18 @@ public class Sample {
         Utilmethods.responsetime(url);
 
     }
+
+    @org.testng.annotations.Test
+    public void apiTest(){
+        ExtentHtmlReporter reporter=new ExtentHtmlReporter("./Reports/test.html");
+        ExtentReports extent=new ExtentReports();
+        extent.attachReporter(reporter);
+        ExtentTest logger = extent.createTest("apitest");
+        logger.log(Status.INFO,"API CALL");
+        logger.log(Status.PASS,"API TEST PASSED");
+        extent.flush();
+
+    }
+
 
 }
